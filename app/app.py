17854101523b3fbc3bd42e8dc70db541d8a26453
Mihir Limbad <151897310+0xAshura@users.noreply.github.com>
2025-04-11 16:28:22 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,20 +1,9 @@
 from flask import Flask
-import socket
-
 app = Flask(_name_)
-hostname = socket.gethostname()
-ip_address = socket.gethostbyname(hostname)
 
 @app.route('/')
-def hello_cloud():
-  return 'Hello from Mihir Limbad - 100938484!! This is Flask running on ECS Fargate!'
-  
-@app.route('/host')
-def host_name():
-  return hostname
+def hello():
+    return "Hello from Mihir Limbad - 100938484!! This is Flask running on ECS Fargate!"
 
-@app.route('/ip')
-def host_ip():
-  return ip_address
-
-app.run(host='0.0.0.0')
+if _name_ == "_main_":
+    app.run()
