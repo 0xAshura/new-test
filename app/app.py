6316b1e82e0b783acf3598e5d6d20562b9f2e899
Mihir Limbad <151897310+0xAshura@users.noreply.github.com>
2025-04-11 15:18:05 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask running on ECS Fargate!"
+    return "Hello from Mihir Limbad - 100938484!! This is Flask running on ECS Fargate!"
 
 if __name__ == "__main__":
     app.run()
